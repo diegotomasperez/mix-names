5872976e2d2805df2ffee6dd932e1d18e0745069
Diego Tomas Perez <diegotoamsperez@usal.es>
2018-08-01 02:26:05 +0200

    codigo para mezclar nombres

---
+++ mixNames.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int main( int argc, const char* argv[] )
+{
+	char *strs[] = {"j","je", "sus", "jes", "edu", "ardo", "edua" "cor","dova","cordo", "lait","herz"};
+	char *strs2[] = {"d", "die", "ego", "to", "tom","mas","per","rez","ez"};
+	
+	int i;
+	int j;
+	char nombre[20];
+	
+	FILE *f = fopen("names.txt", "w");
+	if (f == NULL)
+	{
+	    printf("Error opening file!\n");
+	    return 0;
+	}
+	
+	// Prints each argument on the command line.
+	for(i = 0; i < 11; i++ )
+	{
+		for(j = 0; j < 8; j++){
+
+			fprintf(f, "%s%s\n", strs[i], strs2[j]);
+		}	
+	}
+	
+	for(i = 0; i < 8; i++ )
+	{
+		for(j = 0; j < 11; j++){
+
+			fprintf(f, "%s%s\n", strs2[i], strs[j]);
+		}	
+	}
+	
+	fclose(f);
+}
